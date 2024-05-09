ec91ac39ced0f921c3d1374861f569096cd7a0bc
DamirPavlik <damir.pavlik@gmail.com>
2024-05-10 00:56:22 +0200

    feat: added auth controller

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\Controllers;
+
+use app\Models\AuthModel;
+use app\Support\Session;
+use app\Validation\AuthValidator;
+
+class AuthController extends BaseController
+{
+    public function __construct(
+        private readonly AuthModel $authModel = new AuthModel(),
+        private readonly AuthValidator $authValidator = new AuthValidator()
+    ) {}
+
+    public function handleLogout(): void
+    {
+        if (!Session::isUserLoggedIn()) $this->redirectToNotFound();
+
+        Session::userLogout();
+        $this->redirect(path: '/register');
+    }
+
+    public function handleRegistration(array $params = []): void
+    {
+        $isValid = $this->authValidator->validateRegister($params);
+
+        if (!$isValid) {
+            $_SESSION['registerErrors'] = $this->authValidator->getValidationErrors();
+            $_SESSION['registrationFormData'] = $_POST;
+            $this->redirect('/register');
+        }
+
+        unset($_SESSION['registrationFormData']);
+        $this->authModel->register($_POST);
+        $this->redirect(path: '/');
+    }
+
+    public function handleLogin(array $params = []): void
+    {
+        $isValid = $this->authValidator->validateLogin($params);
+
+        if (!$isValid) {
+            $_SESSION['validationErrors'] = $this->authValidator->getValidationErrors();
+            $_SESSION['loginFormData'] = $_POST;
+            $this->redirect('/login');
+        }
+
+        unset($_SESSION['loginFormData']);
+        $this->authModel->login(email: $params['email'], password: $params['password']);
+        $this->redirect(path: '/');
+    }
+}
