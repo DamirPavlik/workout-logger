967569628fdfa2c975bfd58a887ce8c2a8516a21
DamirPavlik <damir.pavlik@gmail.com>
2024-05-21 07:07:23 +0200

    feat: added a few new js fns

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,32 +1,52 @@
 // Edit Workout Day Title
-let editButton = document.querySelectorAll(".editWorkoutDayTitle");
-editButton.forEach((button) => {
+let editWorkoutDayButton = document.querySelectorAll(".editWorkoutDayTitle");
+let editExerciseButton = document.querySelectorAll(".editExercise");
+function edit(editButton, titleSelector) {
+    editButton.forEach((button) => {
+        button.addEventListener("click", e => {
+            e.preventDefault();
+            let parentEl1 = button.parentElement;
+            let parentEl = parentEl1.parentElement;
+            let title = parentEl.querySelector(titleSelector);
+            let form = parentEl.querySelector("form");
+            let input = form.querySelector("input");
+            input.value = title.textContent;
+            title.classList.add("d-none");
+            form.classList.remove("d-none")
+            console.log(title)
+        })
+    })
+}
+
+edit(editWorkoutDayButton, ".workoutDayTitle")
+edit(editExerciseButton, ".exerciseTitle")
+
+// Dialog
+let dialogButtons = document.querySelectorAll('.showDialog');
+
+dialogButtons.forEach(button => {
+    let workoutCard = button.parentElement;
+    let dialog = workoutCard.querySelector("dialog");
+    let closeButton = dialog.querySelector(".closeButton");
     button.addEventListener("click", e => {
-        e.preventDefault();
-        let parentEl1 = button.parentElement;
-        let parentEl = parentEl1.parentElement;
-        let title = parentEl.querySelector(".workoutDayTitle");
-        let form = parentEl.querySelector("form");
-        let input = form.querySelector("input");
-        input.value = title.textContent;
-        title.classList.add("d-none");
-        form.classList.remove("d-none")
-        console.log(title)
+        dialog.showModal();
+    })
+
+    closeButton.addEventListener("click", e => {
+        dialog.close();
     })
 })
 
-// Dialog
-let dialogButton = document.querySelector('.showDialog');
-let dialog = document.querySelector('.logDialog');
-let dialogCloseButton = document.querySelector('.logDialog .closeButton');
+// Remove workout day confirm
+function confirmSubmit(message) {
+    return confirm(message);
+}
 
-dialogButton.addEventListener('click', e => {
-    dialog.showModal();
-});
+// Date picker default value to current date
+const currentUrl = window.location.href
+const url = new URL(currentUrl);
+const workoutDayCards = document.querySelectorAll(".workoutDayWrapper .workoutCard");
 
-dialogCloseButton.addEventListener('click', e => {
-    dialog.close();
-});
-
-// Date picker default
-document.querySelector("#date").valueAsDate = new Date()
+if (url.pathname.endsWith("/workout-day") && workoutDayCards.length !== 0) {
+    document.querySelector("#date").valueAsDate = new Date()
+}
