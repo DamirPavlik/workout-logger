afdb048895e02e1a6a8d43d6d321fa406fc48a6e
DamirPavlik <damir.pavlik@gmail.com>
2024-05-30 01:54:10 +0200

    refactor: new css version cuz cache

--- view/components/head.php
+++ view/components/head.php
@@ -15,7 +15,7 @@ $parts = parse_url($currentUrl);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Workout Logger</title>
-    <link rel="stylesheet" href="/workout-logger/public/assets/styles/main.css?v=420691">
+    <link rel="stylesheet" href="/workout-logger/public/assets/styles/main.css?v=420692">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
