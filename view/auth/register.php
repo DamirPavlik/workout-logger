837ce127431c7c54e31413fb745c2b48db7efba1
DamirPavlik <damir.pavlik@gmail.com>
2024-06-02 00:15:39 +0200

    feat: update register view

--- view/auth/register.php
+++ view/auth/register.php
@@ -1,50 +1,50 @@
 <?php
 include __DIR__ . "/../components/head.php";
 
-$errors = $_SESSION['validationErrors'] ?? null;
+$errors = $_SESSION['registerErrors'] ?? null;
 $formData = $_SESSION['registrationFormData'] ?? [];
 
-unset($_SESSION["validationErrors"]);
+unset($_SESSION["registerErrors"]);
 ?>
 
-<main>
-    <form method="POST" action="/workout-logger/register/submit">
+<main class="authContainer">
+    <div class="authBox">
         <div>
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" value="<?= $formData['email'] ?? ''; ?>">
+            <h3 class="text-center fw-bold">Register</h3>
+            <form method="POST" action="/workout-logger/register/submit">
+                <div>
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" value="<?= $formData['email'] ?? ''; ?>" placeholder="Email">
+                </div>
+                    <?php if (isset($errors['email'])) : ?>
+                        <p><?= $errors['email'] ?></p>
+                    <?php endif; ?>
+                 <div>
+                    <label for="username">Username</label>
+                    <input type="text" name="username" id="username" value="<?= $formData['username'] ?? ''; ?>" placeholder="Username">
+                </div>
+                    <?php if (isset($errors['username'])) : ?>
+                        <p><?= $errors['username'] ?></p>
+                    <?php endif; ?>
+                <div>
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Password">
+                </div>
+                    <?php if (isset($errors['password'])) : ?>
+                        <p><?= $errors['password'] ?></p>
+                    <?php endif; ?>
+                <div>
+                    <label for="confirm_password">Confirm Password</label>
+                    <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Password">
+                </div>
+                    <?php if (isset($errors['confirm_password'])) : ?>
+                        <p><?= $errors['confirm_password'] ?></p>
+                    <?php endif; ?>
+                <button type="submit" class="mt-2 btn">Submit</button>
+            </form>
+            <p class="text-center mt-4">Already have an account? <a href="/workout-logger/login" class="link">Login.</a></p>
         </div>
-        <?php if (isset($errors['email'])) : ?>
-            <?php foreach ($errors['email'] as $error) : ?>
-                <p><?= $error ?></p>
-            <?php endforeach; ?>
-        <?php endif; ?>
-         <div>
-            <label for="username">Username</label>
-            <input type="text" name="username" id="username" value="<?= $formData['username'] ?? ''; ?>">
-        </div>
-        <?php if (isset($errors['username'])) : ?>
-            <?php foreach ($errors['username'] as $error) : ?>
-                <p><?= $error ?></p>
-            <?php endforeach; ?>
-        <?php endif; ?>
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password">
-        </div>
-        <?php if (isset($errors['password'])) : ?>
-            <?php foreach ($errors['password'] as $error) : ?>
-                <p><?= $error ?></p>
-            <?php endforeach; ?>
-        <?php endif; ?>
-        <div>
-            <label for="confirm_password">Confirm Password</label>
-            <input type="password" name="confirm_password" id="confirm_password">
-        </div>
-        <?php if (isset($errors['confirm_password'])) : ?>
-            <?php foreach ($errors['confirm_password'] as $error) : ?>
-                <p><?= $error ?></p>
-            <?php endforeach; ?>
-        <?php endif; ?>
-        <button type="submit">Submit</button>
-    </form>
+    </div>
 </main>
+
+<?php include __DIR__ . "/../components/footer.php"; ?>
